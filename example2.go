71f0c3cd135e4ebe4401c4ab714902d877394571
Toan Pham <ductoan.uit@gmail.com>
2025-04-03 10:31:39 +0000

    example 2

---
+++ example2.go
@@ -0,0 +1,19 @@
+package main
+import (
+	"fmt"
+	"time"
+)
+
+var c chan int
+func ready(w string, sec int){
+	time.Sleep(time.Duration(sec)*time.Second)
+	fmt.Println(w,"is ready")
+}
+func main(){
+	c = make(chan int)
+	go ready("tea",2)
+	go ready("coffee",1)
+	fmt.Println("Im waiting for but too long")
+	<-c
+	<-c	
+}
