fc39552601faf2f4e389be71826251f3978299fb
Toan Pham <ductoan.uit@gmail.com>
2025-04-03 12:16:18 +0000

    refactor example4: enhance shower function with quit channel for graceful termination

--- example4.go
+++ example4.go
@@ -2,18 +2,23 @@ package main
 
 import "fmt"
 
-
-func shower(c chan int) {
-	for {
-		j := <-c
-		fmt.Printf("%d\n", j)
-	}
-}
-
 func main() {
 	ch := make(chan int)
-	go shower(ch)
+	quit := make(chan bool)
+	go shower(ch, quit)
 	for i := 0; i < 10; i++ {
 		ch <- i
+	}
+	quit <- false	// or true, does not matter
+}
+
+func shower(c chan int, quit chan bool) {
+	for {
+		select {
+		case j := <-c:
+			fmt.Printf("%d\n", j)
+		case <-quit:
+			break
+		}
 	}
 }
